937b36ac9f9841422d003091ce1f2c7080d900b3
BigManzai <39168019+BigManzai@users.noreply.github.com>
2019-07-09 12:31:19 +0200

    09.07.2019

---
+++ oswp-splash/views/splash-admin.php
@@ -0,0 +1,192 @@
+<!-- This file is used to markup the administration form of the widget. -->
+
+
+
+<!-- Ab hier die Ausgabe im Control-Fenster des Adminbereiches -->
+<?php if (!isset($_POST['ossplashkonfig'])): ?>
+
+<?php
+// Gettext einfügen
+/* Make theme available for translation */
+	load_plugin_textdomain( 'oswp-splash', false, basename( dirname( __FILE__ ) ) . '/lang' );
+ ?>
+ 
+<!-- Start Abfrage Nutzer -->
+<form class="container" action="" method="post">
+    <input type="hidden" name="ossplashkonfig" value="1" />
+		
+<!-- OpenSim Einstellungen --> 
+<!-- $CONF_os_name, $CONF_db_server, $CONF_db_user, $CONF_db_pass, $CONF_db_database -->
+
+<h1>OSWP-Splash</h1>
+<!-- Scheckboxen noch ohne echte abfrage -->
+	<div class="row section">
+        <div class="row"><p><input class="input border" type="checkbox"  checked="checked" name="CONF_os_totalUsers"/>
+		<label for="base" class="validate"><?php echo esc_html__( 'Users in the grid', 'oswp-splash' ); ?></b></label>
+        </div>
+    </div>
+	<div class="row section">
+        <div class="row"><p><input class="input border" type="checkbox"  checked="checked" name="CONF_os_activeUsers"/>
+		<label for="base" class="validate"><?php echo esc_html__( 'Active in the last 30 days', 'oswp-splash' ) ; ?></b></label>
+        </div>
+    </div>
+	<div class="row section">
+        <div class="row"><p><input class="input border" type="checkbox"  checked="checked" name="CONF_os_totalGridAccounts"/>
+		<label for="base" class="validate"><?php echo esc_html__( 'Hypergrid users', 'oswp-splash' ) ; ?></b></label>
+        </div>
+    </div>
+	<div class="row section">
+        <div class="row"><p><input class="input border" type="checkbox"  checked="checked" name="CONF_os_totalAccounts"/>
+		<label for="base" class="validate"><?php echo esc_html__( 'Registrations', 'oswp-splash' ) ; ?></b></label>
+        </div>
+    </div>
+	<div class="row section">
+        <div class="row"><p><input class="input border" type="checkbox"  checked="checked" name="CONF_os_friends"/>
+		<label for="base" class="validate"><?php echo esc_html__( 'Active friendships', 'oswp-splash' ) ; ?></b></label>
+        </div>
+    </div>
+	<div class="row section">
+        <div class="row"><p><input class="input border" type="checkbox"  checked="checked" name="CONF_os_groups"/>
+		<label for="base" class="validate"><?php echo esc_html__( 'Active groups', 'oswp-splash' ) ; ?></b></label>
+        </div>
+    </div>
+	<div class="row section">
+        <div class="row"><p><input class="input border" type="checkbox"  checked="checked" name="CONF_os_normalregions"/>
+		<label for="base" class="validate"><?php echo esc_html__( 'Normal regions', 'oswp-splash' ) ; ?></b></label>
+        </div>
+    </div>
+	<div class="row section">
+        <div class="row"><p><input class="input border" type="checkbox"  checked="checked" name="CONF_os_varregions"/>
+		<label for="base" class="validate"><?php echo esc_html__( 'Var regions', 'oswp-splash' ) ; ?></b></label>
+        </div>
+    </div>
+	<div class="row section">
+        <div class="row"><p><input class="input border" type="checkbox"  checked="checked" name="CONF_os_totalRegions"/>
+		<label for="base" class="validate"><?php echo esc_html__( 'Total regions', 'oswp-splash' ) ; ?></b></label>
+        </div>
+    </div>
+	<div class="row section">
+        <div class="row"><p><input class="input border" type="checkbox"  checked="checked" name="CONF_os_regionsergebnis"/>
+		<label for="base" class="validate" ><?php echo esc_html__( 'Regions in m²', 'oswp-splash' ) ; ?></b></label>
+        </div>
+    </div>
+<!-- Scheckboxen noch ohne echte abfrage -->
+
+	
+<!-- Abfragen Zugang -->
+	<div class="row section">
+    <p><label for="base" class="label control-label"><i class="dashicons dashicons-book" style="font-size:20px"></i><?php echo esc_html__( '  Grid Name or Headline:', 'oswp-splash' ) ; ?></b></label></p>
+        <div class="row">
+            <p><input class="input border" type="text" placeholder="My OpenSim" name="CONF_os_name"/></p>
+        </div>
+    </div>
+	
+	<div class="row section">	
+    <p><label for="base" class="label control-label"><i class="dashicons dashicons-book" style="font-size:20px"></i><?php echo esc_html__( '  MySQL Server IP:', 'oswp-splash' ) ; ?></b></label></p>
+        <div class="row">
+            <p><input class="input border" type="text" placeholder="127.0.0.1" name="CONF_db_server"/></p>
+        </div>
+    </div>
+ 
+	<div class="row section">
+    <p><label for="base" class="label control-label"><i class="dashicons dashicons-book" style="font-size:20px"></i><?php echo esc_html__( '  MySQL Database:', 'oswp-splash' ) ; ?></b></label></p>
+        <div class="row">
+            <p><input class="input border" type="text" placeholder="opensim" name="CONF_db_database"/></p>
+        </div>
+    </div>
+
+ 	<div class="row section">
+    <p><label for="base" class="label control-label"><i class="dashicons dashicons-book" style="font-size:20px"></i><?php echo esc_html__( '  MySQL User:', 'oswp-splash' ) ; ?></b></label></p>
+        <div class="row">
+            <p><input class="input border" type="text" placeholder="opensim" name="CONF_db_user"/></p>
+        </div>
+    </div>
+	
+ 	<div class="row section">
+    <p><label for="base" class="label control-label"><i class="dashicons dashicons-book" style="font-size:20px"></i><?php echo esc_html__( '  Password:', 'oswp-splash' ) ; ?></b></label></p>
+        <div class="row">
+            <p><input class="input border" type="password" placeholder="password" name="CONF_db_pass"/></p>
+        </div>
+    </div>
+<!-- Abfragen Zugang -->
+<?php endif ?>
+
+<?php
+//print_r($_POST);
+	if (isset($_POST['ossplashkonfig']) AND $_POST['ossplashkonfig'] == 1)
+	{
+		// wir schaffen unsere Variablen
+		//$CONF_os_name, $CONF_db_server, $CONF_db_user, $CONF_db_pass, $CONF_db_database 
+
+		$CONF_os_name  = $_POST['CONF_os_name']; //variable name, string value use: %s
+		$CONF_db_server  = $_POST['CONF_db_server']; //server http or IP, string value use: %s
+		$CONF_db_user  = $_POST['CONF_db_user']; //database user name, string value use: %s
+		$CONF_db_pass  = $_POST['CONF_db_pass']; //database password, string value use: %s
+		$CONF_db_database  = $_POST['CONF_db_database']; //database name, string value use: %s
+		
+		$CONF_os_totalUsers  = $_POST['CONF_os_totalUsers'];
+		$CONF_os_activeUsers  = $_POST['CONF_os_activeUsers'];
+		$CONF_os_totalGridAccounts  = $_POST['CONF_os_totalGridAccounts'];
+		$CONF_os_totalAccounts  = $_POST['CONF_os_totalAccounts'];
+		$CONF_os_friends  = $_POST['CONF_os_friends'];
+		$CONF_os_groups  = $_POST['CONF_os_groups'];
+		$CONF_os_normalregions  = $_POST['CONF_os_normalregions'];
+		$CONF_os_varregions  = $_POST['CONF_os_varregions'];
+		$CONF_os_totalRegions  = $_POST['CONF_os_totalRegions'];
+		$CONF_os_regionsergebnis  = $_POST['CONF_os_regionsergebnis'];
+		
+		global $wpdb;
+		// Fehler anzeigen
+		//$wpdb->show_errors();
+		
+		// Tabellen Name
+		$tablename = $wpdb->prefix . "ossplash";
+		
+		//Daten werden sortiert
+		$charset_collate = $wpdb->get_charset_collate();
+		
+		// Alle alten Tabelleneinträge löschen
+/* 		$sql = "DELETE FROM $tablename WHERE id=*";
+
+		if ($conn->query($sql) === TRUE) {
+			echo "Eintrag wurde gelöscht.";
+		} else {
+			echo "Beim löschen ist folgender Fehler aufgetreten: " . $conn->error;
+		} */
+
+		// Neue Tabelleneinträge eintragen NEU: os_id mediumint (9) NOT NULL,
+		$sql = "CREATE TABLE $tablename (
+		  os_id mediumint (9) NOT NULL,
+		  CONF_os_name text NOT NULL,
+		  CONF_db_server text NOT NULL,
+		  CONF_db_user text NOT NULL,
+		  CONF_db_pass text NOT NULL,
+		  CONF_db_database text NOT NULL,
+		  CONF_os_totalUsers text NOT NULL, 
+		  CONF_os_activeUsers text NOT NULL, 
+		  CONF_os_totalGridAccounts text NOT NULL, 
+		  CONF_os_totalAccounts text NOT NULL, 
+		  CONF_os_friends text NOT NULL, 
+		  CONF_os_groups text NOT NULL, 
+		  CONF_os_normalregions text NOT NULL, 
+		  CONF_os_varregions text NOT NULL, 
+		  CONF_os_totalRegions text NOT NULL, 
+		  CONF_os_regionsergebnis text NOT NULL,
+		  PRIMARY KEY  (os_id)
+		) $charset_collate;";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+		
+		// NEU: Erst Tabellen löschen dann schreiben os_id nicht vergessen.
+		$wpdb->delete( $tablename, array( 'os_id' => 0 ) );
+			
+		// Eigentliche Daten speichern
+		$sql2 = $wpdb->prepare("INSERT INTO $tablename (
+		CONF_os_name, CONF_db_server, CONF_db_user, CONF_db_pass, CONF_db_database, CONF_os_totalUsers, CONF_os_activeUsers, CONF_os_totalGridAccounts, CONF_os_totalAccounts, CONF_os_friends, CONF_os_groups, CONF_os_normalregions, CONF_os_varregions, CONF_os_totalRegions, CONF_os_regionsergebnis) 
+		values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", 
+		$CONF_os_name, $CONF_db_server, $CONF_db_user, $CONF_db_pass, $CONF_db_database, $CONF_os_totalUsers, $CONF_os_activeUsers, $CONF_os_totalGridAccounts, $CONF_os_totalAccounts, $CONF_os_friends, $CONF_os_groups, $CONF_os_normalregions, $CONF_os_varregions, $CONF_os_totalRegions, $CONF_os_regionsergebnis);
+		dbDelta( $sql2 );		
+	}
+
+?>
---
+++ oswp-splash/views/splash-widget.php
@@ -0,0 +1,94 @@
+<?php
+	global $wpdb;
+	// Fehler anzeigen
+	//$wpdb->show_errors();
+	
+	// Tabellenname erstellen
+	$tablename = $wpdb->prefix . "ossplash";
+	
+	// Auslesen der wp datenbank
+	$CONF_db_serveross = $wpdb->get_var( "SELECT CONF_db_server FROM $tablename" );
+	$CONF_db_useross = $wpdb->get_var( "SELECT CONF_db_user FROM $tablename" );
+	$CONF_db_passoss = $wpdb->get_var( "SELECT CONF_db_pass FROM $tablename" );
+	$CONF_db_databaseoss = $wpdb->get_var( "SELECT CONF_db_database FROM $tablename" );
+	$CONF_os_nameoss = $wpdb->get_var( "SELECT CONF_os_name FROM $tablename" );
+	
+	$CONF_os_totalUsersos = $wpdb->get_var( "SELECT CONF_os_totalUsers FROM $tablename" );
+	$CONF_os_activeUsersos = $wpdb->get_var( "SELECT CONF_os_activeUsers FROM $tablename" );
+	$CONF_os_totalGridAccountsos = $wpdb->get_var( "SELECT CONF_os_totalGridAccounts FROM $tablename" );
+	$CONF_os_totalAccountsos = $wpdb->get_var( "SELECT CONF_os_totalAccounts FROM $tablename" );
+	$CONF_os_friendsos = $wpdb->get_var( "SELECT CONF_os_friends FROM $tablename" );
+	$CONF_os_groupsos = $wpdb->get_var( "SELECT CONF_os_groups FROM $tablename" );
+	$CONF_os_normalregionsos = $wpdb->get_var( "SELECT CONF_os_normalregions FROM $tablename" );
+	$CONF_os_varregionsos = $wpdb->get_var( "SELECT CONF_os_varregions FROM $tablename" );
+	$CONF_os_totalRegionsos = $wpdb->get_var( "SELECT CONF_os_totalRegions FROM $tablename" );
+	$CONF_os_regionsergebnisos = $wpdb->get_var( "SELECT CONF_os_regionsergebnis FROM $tablename" );
+	  
+$con = mysqli_connect($CONF_db_serveross,$CONF_db_useross,$CONF_db_passoss,$CONF_db_databaseoss);
+
+// Query the database and get the count
+
+$resultoss1 = mysqli_query($con,"SELECT COUNT(*) FROM Presence") or die("Error: " . mysqli_error($con));
+list($totalUsers) = mysqli_fetch_row($resultoss1);
+
+$resultoss2 = mysqli_query($con,"SELECT COUNT(*) FROM regions") or die("Error: " . mysqli_error($con));
+list($totalRegions) = mysqli_fetch_row($resultoss2);
+
+$resultoss3 = mysqli_query($con,"SELECT COUNT(*) FROM UserAccounts") or die("Error: " . mysqli_error($con));
+list($totalAccounts) = mysqli_fetch_row($resultoss3);
+
+$resultoss4 = mysqli_query($con,"SELECT COUNT(*) FROM GridUser WHERE Login > (UNIX_TIMESTAMP() - (30*86400))") or die("Error: " . mysqli_error($con));
+list($activeUsers) = mysqli_fetch_row($resultoss4);
+
+$resultoss5 = mysqli_query($con,"SELECT COUNT(*) FROM GridUser") or die("Error: " . mysqli_error($con));
+list($totalGridAccounts) = mysqli_fetch_row($resultoss5);
+// NEU
+$resultoss6 = mysqli_query($con," SELECT COUNT(*) FROM regions WHERE sizeX = 256 AND sizeY = 256") or die("Error: " . mysqli_error($con));
+list($normalregions) = mysqli_fetch_row($resultoss6);
+
+$resultoss7 = mysqli_query($con," SELECT COUNT(*) FROM regions WHERE sizeX <> 256 AND sizeY <> 256") or die("Error: " . mysqli_error($con));
+list($varregions) = mysqli_fetch_row($resultoss7);
+
+$resultoss8 = mysqli_query($con,"select sum(sizeX) as summe from regions") or die("Error: " . mysqli_error($con));
+list($regions1) = mysqli_fetch_row($resultoss8);
+$regionsergebnis = $regions1*$regions1;
+
+$resultoss9 = mysqli_query($con,"SELECT COUNT(*) FROM Friends") or die("Error: " . mysqli_error($con));
+list($friends) = mysqli_fetch_row($resultoss9);
+
+$resultoss10 = mysqli_query($con,"SELECT COUNT(*) FROM os_groups_groups") or die("Error: " . mysqli_error($con));
+list($groups) = mysqli_fetch_row($resultoss10);
+
+// Gettext einfügen
+/* Make theme available for translation */
+	load_plugin_textdomain( 'oswp-splash', false, basename( dirname( __FILE__ ) ) . '/lang' );
+
+// Display the results
+echo "<h1>$CONF_os_nameoss</h1>";
+if ($CONF_os_totalUsersos == on){ echo esc_html__( 'Users in the grid : ', 'oswp-splash' ). $totalUsers ."<br>"; }else{	echo ""; }
+if ($CONF_os_activeUsersos == on){ echo esc_html__( 'Active in the last 30 days : ', 'oswp-splash' ). $activeUsers ."<br>"; }else{	echo ""; } 
+if ($CONF_os_totalGridAccountsos == on){ echo esc_html__( 'Hypergrid users : ', 'oswp-splash' ). $totalGridAccounts ."<br>"; }else{	echo ""; } 
+if ($CONF_os_totalAccountsos == on){ echo esc_html__( 'Registrations : ', 'oswp-splash' ). $totalAccounts ."<br>"; }else{	echo ""; } 
+if ($CONF_os_friendsos == on){ echo esc_html__( 'Active friendships : ', 'oswp-splash' ). $friends ."<br>"; }else{	echo ""; } 
+if ($CONF_os_groupsos == on){ echo esc_html__( 'Active groups : ', 'oswp-splash' ). $groups ."<br>"; }else{	echo ""; } 
+if ($CONF_os_normalregionsos == on){ echo esc_html__( 'Normal regions : ', 'oswp-splash' ). $normalregions ."<br>"; }else{	echo ""; } 
+if ($CONF_os_varregionsos == on){ echo esc_html__( 'Var regions : ', 'oswp-splash' ). $varregions ."<br>"; }else{	echo ""; } 
+if ($CONF_os_totalRegionsos == on){ echo esc_html__( 'Total regions : ', 'oswp-splash' ). $totalRegions ."<br>"; }else{	echo ""; } 
+if ($CONF_os_regionsergebnisos == on){ echo esc_html__( 'Regions in m² : ', 'oswp-splash' ). $regionsergebnis ."<br>"; }else{	echo ""; } 
+
+// Online? Offline?
+$check = $wpdb->get_var( "SELECT * FROM regions LIMIT 0,1" );
+	if ($check){
+	// Keine Region vorhanden.
+		echo "<h1><font color=#AA0000>".esc_html__( 'Grid is OFFLINE', 'oswp-splash' )."</font></h1></b><br>";
+	}else{
+	// Regionen sind da.
+		echo "<h1><font color=#00AA00>".esc_html__( 'Grid is ONLINE', 'oswp-splash' )."</font></h1></b><br>";
+	} 
+	
+// Alles schliessen
+	mysqli_close($con);
+	$wpdb->flush(); //Clearing the Cache
+?>
+
+
